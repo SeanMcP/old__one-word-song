e8fb944ea6a0aabfb937b6b14899f0a55bd2ac1d
Sean McPherson <seanmcp25@gmail.com>
2019-01-30 21:13:39 -0500

    Add prettier

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
 if (process.env.NODE_ENV === 'development') {
-    const axe = require('react-axe');
-    axe(React, ReactDOM, 1000);
+  const axe = require('react-axe')
+  axe(React, ReactDOM, 1000)
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
