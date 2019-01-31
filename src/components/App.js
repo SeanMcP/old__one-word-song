bf4ee6baae5dfbd14e52e4ff56088e63837a1cf3
Sean McPherson <seanmcp25@gmail.com>
2019-01-30 21:17:09 -0500

    Remove boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const App = () => {
+  return <div>One word song</div>
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+import App from './components/App'
 import * as serviceWorker from './serviceWorker'
 
 if (process.env.NODE_ENV === 'development') {
@@ -11,7 +11,4 @@ if (process.env.NODE_ENV === 'development') {
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister()
