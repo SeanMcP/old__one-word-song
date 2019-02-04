400c23393490135e611103c6ae5eb876f1af4f8e
Sean McPherson <seanmcp25@gmail.com>
2019-02-04 06:38:12 -0500

    Rework game mechanism

---
+++ src/hooks/useGame.js
@@ -0,0 +1,16 @@
+// import React from 'react'
+
+import useRounds from './useRounds'
+import useScore from './useScore'
+
+const useGame = (rounds=3) => {
+    const ROUNDS = useRounds(rounds)
+    const SCORE = useScore()
+
+    return {
+        ...ROUNDS,
+        ...SCORE
+    }
+}
+
+export default useGame
--- src/hooks/useScore.js
+++ src/hooks/useScore.js
@@ -1,31 +1,20 @@
 import React from 'react'
 
-const useScore = (numberOfTeams = 2, numberOfRounds = 3) => {
-  const initialState = {}
-  const rounds = generateRoundsObject(numberOfRounds)
-  for (let i = 0; i < numberOfTeams; i++) {
-    initialState[i] = rounds
+const useScore = () => {
+  const initialState = {
+    0: [],
+    1: []
   }
   const [score, setScore] = React.useState(initialState)
-  const updateScore = (id, round, value) => {
-    const teamsCopy = { ...score }
-    const roundsCopy = { ...teamsCopy[id] }
-    roundsCopy[round] = roundsCopy[round] + value
-    teamsCopy[id] = roundsCopy
-    setScore(teamsCopy)
+  const updateScore = (id, value) => {
+    const copy = { ...score }
+    copy[id].push(value)
+    setScore(copy)
   }
   return {
     score,
     updateScore
   }
-}
-
-function generateRoundsObject(number) {
-  const output = {}
-  for (let i = 1; i > number; i++) {
-    output[i] = 0
-  }
-  return output
 }
 
 export default useScore
