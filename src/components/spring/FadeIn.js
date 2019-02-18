95bfbf1110ca86290b26da174b18dca9f09a24d4
Sean McPherson <seanmcp25@gmail.com>
2019-02-18 06:54:25 -0500

    Set default when destructuring props

--- src/components/spring/FadeIn.js
+++ src/components/spring/FadeIn.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import { animated, useSpring } from 'react-spring'
 
-const FadeIn = ({ children, delay }) => {
+const FadeIn = ({ children, delay = 0 }) => {
   const styles = useSpring({
     from: { opacity: 0 },
     to: { opacity: 1 },
     delay
   })
   return <animated.span style={styles}>{children}</animated.span>
-}
-
-FadeIn.defaultProps = {
-  delay: 0
 }
 
 export default FadeIn
